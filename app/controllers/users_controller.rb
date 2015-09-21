58200f82e9fa706ea8909a477f176c5244015b80
kazuqi <kazuki@gmail.com>
2015-09-21 12:18:12 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
+  # 課題1 ここから
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+            redirect_to @user , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
+  # 課題1 ここまで
+
   def show # 追加
     @user = User.find(params[:id])
   end
@@ -22,7 +40,11 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :profile, :area)  #課題1
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
---
+++ db/migrate/20150921104353_addarea_to_user.rb
@@ -0,0 +1,4 @@
+class AddareaToUser < ActiveRecord::Migration
+  def change
+  end
+end
